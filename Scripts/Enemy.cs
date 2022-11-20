c8566db6003e0aeba50af3aae2b50649867323bb
Christoph Prenissl <chris@celrage.com>
2022-11-20 16:20:05 +0100

    enemy now moves towards player

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -4,28 +4,42 @@ namespace TwentySecondGameJam2022.Scripts
 {
     public class Enemy : KinematicBody2D
     {
+        private enum EnemyState
+        {
+            Idle,
+            Moving
+        }
+        
         [Export] public int Health;
         [Export] public float ShootInterval;
         [Export] public float LookingIndicatorDistance = 30;
+        [Export] public float MinDistanceToPlayer = 30;
+        [Export] public int Speed = 100;
         [Export] public int BulletSpeed = 200;
         [Export] public int BulletMaxRange = 1200;
         [Export] public PackedScene Bullet;
 
+        [Export] public NodePath BodySpritePath;
         [Export] public NodePath HitDetectionAreaPath;
         [Export] public NodePath LookingDirectionSpritePath;
         [Export] public NodePath HealthBarPath;
 
         private Player _player;
+        private AnimatedSprite _bodySprite;
         private Area2D _hitDetectionArea;
         private Sprite _lookingDirectionIndicator;
         private HealthBar _healthBar;
 
+        private EnemyState _state;
+        private Vector2 _velocity;
         private float _currentTimeToShoot;
         
         public override void _Ready()
         {
+            _state = EnemyState.Idle;
             _player = (Player) GetTree().CurrentScene.FindNode(nameof(Player));
-            
+
+            _bodySprite = GetNode<AnimatedSprite>(BodySpritePath);
             _hitDetectionArea = GetNode<Area2D>(HitDetectionAreaPath);
             _lookingDirectionIndicator = GetNode<Sprite>(LookingDirectionSpritePath);
             _healthBar = GetNode<HealthBar>(HealthBarPath);
@@ -33,10 +47,11 @@ namespace TwentySecondGameJam2022.Scripts
             _healthBar.SetMaxHealth(Health);
             _currentTimeToShoot = ShootInterval;
 
-            _hitDetectionArea.Connect("area_entered", this, nameof(Hit));
+            _hitDetectionArea.Connect("area_entered", this, nameof(GotHit));
+            SetState(EnemyState.Idle);
         }
 
-        public override void _Process(float delta)
+        public override void _PhysicsProcess(float delta)
         {
             if (_currentTimeToShoot <= 0)
             {
@@ -44,9 +59,31 @@ namespace TwentySecondGameJam2022.Scripts
                 _currentTimeToShoot = ShootInterval;
             }
 
-            _lookingDirectionIndicator.Position = Position.DirectionTo(_player.GlobalPosition) * LookingIndicatorDistance;
+            var directionToPlayer = Position.DirectionTo(_player.GlobalPosition);
 
+            _lookingDirectionIndicator.Position = directionToPlayer * LookingIndicatorDistance;
+            
+            if (Position.DistanceTo(_player.GlobalPosition) > MinDistanceToPlayer)
+            {
+                SetState(EnemyState.Moving);
+                _velocity = directionToPlayer * Speed;
+                _velocity = MoveAndSlide(_velocity);
+            }
+            else
+            {
+                SetState(EnemyState.Idle);
+            }
+            
             _currentTimeToShoot = Mathf.Max(_currentTimeToShoot - delta, 0);
+        }
+
+        private void SetState(EnemyState state)
+        {
+            if (_state != state)
+            {
+                _state = state;
+                _bodySprite.Animation = state.ToString().ToLower();
+            }
         }
 
         private void ShootBullet()
@@ -61,7 +98,7 @@ namespace TwentySecondGameJam2022.Scripts
             }
         }
 
-        private void Hit(Area2D area)
+        private void GotHit(Area2D area)
         {
             var bullet = (Bullet) area;
 
